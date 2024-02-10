4eebc5dc242871bd67c2997e76694d928494d4f9
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-02-10 21:35:19 +0530

    test - Added scope.js file

---
+++ scope.js
@@ -0,0 +1,31 @@
+/* let num = 100;
+
+function printNum() {
+    const num = 40;
+    console.log(num); //40
+
+
+    if (true) {
+        let num = 50;
+        console.log(num); //50
+    }
+    console.log(num); //40
+
+}
+console.log(num); //100
+printNum()
+ */
+/* 
+let num1 = 10;
+
+if (true) {
+    num1 = 20;
+
+    console.log(num1);
+}
+
+console.log(num1); //outside block also it changes the value. if the global value is accessed and manipulated inside any block.
+ */
+
+console.log(name);
+var name = "joe"
