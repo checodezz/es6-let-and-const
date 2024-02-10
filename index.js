30831692a5932af0231874a1fe197d22ac181658
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-02-10 13:12:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+/*
+ let numericVariable = 34;
+console.log(numericVariable);
+
+let stringVariable = "Hello!, world";
+console.log(stringVariable);
+
+let firstName = "john";
+let lastName = 'Doe';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+let numberVariable = 10;
+console.log(numberVariable);
+numberVariable = 20;
+console.log(numberVariable);
+
+
+let num1 = 16;
+let num2 = 5;
+let sum = num1 + num2;
+let difference = num1 - num2;
+
+console.log(sum);
+console.log(difference);
+
+let newName;
+newName = "chethan";
+console.log(newName);
+ */
+//Const
+const numericConst = 30;
+console.log(numericConst);
+
+const stringConst = 'Hello, Const';
+console.log(stringConst);
+
+// stringConst = "im, reassigning"
+// console.log(stringConst);
+
+//we cant do this with const
+// const newConstVariable
+
+const numericArr = [1, 2, 3, 4, 5];
+console.log(numericArr);
+
+numericArr[0] = 10;
+numericArr[1] = 9;
+numericArr[2] = 8;
+console.log(numericArr);
+
+const obj = {
+    name: "john",
+    age: 25
+}
+
+obj.name = "sid"
+obj.age = 55;
+console.log(obj);
